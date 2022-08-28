94afe1ea684403d053fa8a2ab378d7e595dab5ec
Muhmmed Medhat <muhmmedmedhat0@gmail.com>
2022-08-28 04:25:47 +0200

    adding stripe for payment

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import Annoucement from '../../components/Home/Annoucement';
 import Footer from '../../components/Home/Footer';
 import Navbar from '../../components/Home/Navbar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 import style from '../../styles/Cart/Cart.module.css';
-import {useSelector} from 'react-redux'
+import { useSelector } from 'react-redux'
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios'
 function Cart() {
-  const  cart  = useSelector((state) => state.cart);
+  const cart = useSelector((state) => state.cart);
+  const STRIPE_KEY =
+    'pk_test_51LPnilHHab9eHCh35EPRzuze8DtBPdSOBK1wTI0wU60WmRTYsSKkKn6Tn7QcCf6vO4bpmf6DP1QAInbJIIsS8j7C00TxRuv3Wf';
+  const [stripeToken, setStripeToken] = useState(null);
+  const onToken = (token) => setStripeToken(token);
+  const router = useRouter();
+  // send token to backend
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        const response = await axios.post(`http://localhost:8080/api/payment/`, {
+          amount: cart.totalPrice * 100, token: stripeToken.id,
+        });
+        console.log(response.data);
+        // router.push('/')
+        return response.data;
+      } catch (error) {console.log(error);}
+    };
+    // call the function
+    stripeToken && makeRequest();
+  }, [stripeToken, cart.totalPrice, router]);
   return (
     <section>
       <Navbar />
@@ -23,9 +46,17 @@ function Cart() {
             <span className={style.topText}>Shopping Bag(2)</span>
             <span className={style.topText}>Your Wishlist (0)</span>
           </div>
-          <button style={{ border: 'none', backgroundColor: 'black', color: 'white' }} className={style.topButton} >
-            CHECKOUT NOW
-          </button>
+          <StripeCheckout name="E-Commerce"
+              image="https://img.freepik.com/free-vector/supermarket-logo-concept_23-2148467758.jpg?w=740&t=st=1659300159~exp=1659300759~hmac=75c6925c2fe434a8d477161f0cac8b5b8eeae3680a66178d4cb78f5c15ad95b4"
+              billingAddress shippingAddress
+              description={` your total price is ${cart.totalPrice}`}
+              amount={cart.totalPrice * 100}
+              token={onToken}
+              stripeKey={STRIPE_KEY}>
+              <button style={{ border: 'none', backgroundColor: 'black', color: 'white' }} className={style.topButton} >
+                CHECKOUT NOW
+              </button>
+            </StripeCheckout>
         </div>
         <div className={style.bottom}>
           <div className={style.info}>
@@ -74,7 +105,15 @@ function Cart() {
               <span className={(style.summeryText, style.summeryTotal)}>Total</span>
               <span className={style.summeryPrice}>$ {cart.totalPrice}</span>
             </div>
-            <button className={style.checkNow}>CHECKOUT NOW</button>
+            <StripeCheckout name="E-Commerce"
+              image="https://img.freepik.com/free-vector/supermarket-logo-concept_23-2148467758.jpg?w=740&t=st=1659300159~exp=1659300759~hmac=75c6925c2fe434a8d477161f0cac8b5b8eeae3680a66178d4cb78f5c15ad95b4"
+              billingAddress shippingAddress
+              description={` your total price is ${cart.totalPrice}`}
+              amount={cart.totalPrice * 100}
+              token={onToken}
+              stripeKey={STRIPE_KEY}>
+              <button className={style.checkNow}>CHECKOUT NOW</button>
+            </StripeCheckout>
           </div>
           {/* end summery */}
         </div>
