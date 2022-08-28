01a2140c7d476193ffc21d7dd4edf3bd936de1d9
Muhmmed Medhat <muhmmedmedhat0@gmail.com>
2022-08-28 06:17:24 +0200

    login with redux

--- app/store.js
+++ app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import products from './slices/products';
 import cart from './slices/cart';
+import user from './slices/user';
 export default configureStore({
   reducer: {
     products,
     cart,
+    user,
   },
 });
--- pages/login/index.js
+++ pages/login/index.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Link from 'next/link';
 import style from '../../styles/Login/Login.module.css';
+import { logIn } from '../../app/slices/user';
+import {useDispatch } from 'react-redux';
 
 function Login() {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const info = { email, password };
+  const handleClick = (e) => {
+    e.preventDefault();
+    dispatch(logIn(info));
+}
   return (
     <section className={style.container}>
       <div className={style.wrapper}>
         <h1 className={style.title}>SIGN IN</h1>
         <form action="" className={style.form}>
-          <input type="email" placeholder="email" className={style.input} />
-          <input
-            type="password"
-            placeholder="password"
-            className={style.input}
-          />
+          <input type="email" autoComplete='true' placeholder="email" className={style.input}  onChange={(e)=> setEmail(e.target.value)}/>
+          <input type="password" autoComplete='true' placeholder="password" className={style.input} onChange={(e)=> setPassword(e.target.value)}/>
           <span className={style.agrement}>
             By creating an account, I consent to the processing of my personal
             data in accordance with the <b>PRIVACY POLICY</b>
           </span>
-          <button className={style.button}>LOG IN</button>
+          <button className={style.button} onClick={handleClick}>LOG IN</button>
           <Link className={style.link} href="/register">CREATE A NEW ACCOUNT</Link>
         </form>
       </div>
