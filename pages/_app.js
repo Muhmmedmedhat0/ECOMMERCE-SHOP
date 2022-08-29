8dad8d25e8501e5227bd4a53263f59426f080b55
Muhmmed Medhat <muhmmedmedhat0@gmail.com>
2022-08-29 17:51:55 +0200

    adding redux-persist for saving data in storage

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,15 @@ import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 config.autoAddCss = false;
 import { Provider } from 'react-redux';
-import store from '../app/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from '../app/store';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <PersistGate loding={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
     </Provider>
   );
 }
